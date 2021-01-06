1b0e6d3f8a20040f32d7a619d0065fafd130faf4
habibsumoncse <habibsumoncse@gmail.com>
2021-01-06 15:46:30 +0600

    add login client

--- micro-blogging-frontend/src/App.js
+++ micro-blogging-frontend/src/App.js
@@ -1,32 +1,130 @@
-import React from 'react';
-import AddCategory from './Category/AddCategory';
-import Categorylist from './Category/CategoryList';
-import EditCategory from './Category/EditCategory';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import './App.css';
-function App() {
-  return (
-    <Router>
-      <div className="container">
-        <nav className="navbar navbar-expand-lg navheader">
-          <div className="collapse navbar-collapse" >
-            <ul className="navbar-nav mr-auto">
+import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import AuthService from "./services/auth.service";
+
+import Login from "./components/login.component";
+import Register from "./components/register.component";
+import Home from "./components/home.component";
+import Profile from "./components/profile.component";
+import BoardUser from "./components/board-user.component";
+import BoardModerator from "./components/board-moderator.component";
+import BoardAdmin from "./components/board-admin.component";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.logOut = this.logOut.bind(this);
+
+    this.state = {
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+    };
+  }
+
+  componentDidMount() {
+    const user = AuthService.getCurrentUser();
+    if (user) {
+      this.setState({
+        currentUser: user,
+          // showModeratorBoard: user.roles.includes("ROLE_admin"),
+          showAdminBoard: "ROLE_ADMIN",
+      });
+    }
+  }
+
+  logOut() {
+    AuthService.logout();
+  }
+
+  render() {
+    const { currentUser, showModeratorBoard, showAdminBoard } = this.state;
+
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to={"/"} className="navbar-brand">
+            Micro-Bloggging
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/home"} className="nav-link">
+                Home
+              </Link>
+            </li>
+
+            {showModeratorBoard && (
               <li className="nav-item">
-                <Link to={'/AddCategory'} className="nav-link">AddCategory</Link>
+                <Link to={"/mod"} className="nav-link">
+                  Moderator Board
+                </Link>
               </li>
+            )}
+
+            {showAdminBoard && (
               <li className="nav-item">
-                {/* <Link to={'/Categorylist'} className="nav-link">Category List</Link> */}
+                <Link to={"/admin"} className="nav-link">
+                  Admin Board
+                </Link>
               </li>
-            </ul>
+            )}
+
+            {currentUser && (
+              <li className="nav-item">
+                <Link to={"/user"} className="nav-link">
+                  User
+                </Link>
+              </li>
+            )}
           </div>
-        </nav> <br />
-        <Switch>
-          <Route exact path='/AddCategory' component={AddCategory} />
-          <Route path='/edit/:id' component={EditCategory} /> 
-          <Route path='/Categorylist' component={Categorylist} />
-        </Switch>
+
+          {currentUser ? (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/profile"} className="nav-link">
+                  {currentUser.username}
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a href="/login" className="nav-link" onClick={this.logOut}>
+                  LogOut
+                </a>
+              </li>
+            </div>
+          ) : (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/login"} className="nav-link">
+                  Login
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to={"/register"} className="nav-link">
+                  Sign Up
+                </Link>
+              </li>
+            </div>
+          )}
+        </nav>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/home"]} component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/profile" component={Profile} />
+            <Route path="/user" component={BoardUser} />
+            <Route path="/mod" component={BoardModerator} />
+            <Route path="/admin" component={BoardAdmin} />
+          </Switch>
+        </div>
       </div>
-    </Router>
-  );
+    );
+  }
 }
+
 export default App;
---
+++ micro-blogging-frontend/src/services/auth.service.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8082/oauth/token";
+
+class AuthService {
+  login(username, password) {
+
+    var FormData = require('form-data');
+    var data = new FormData();
+    data.append('username', username);
+    data.append('password', password);
+    data.append('grant_type', 'password');
+
+    var config = {
+      method: 'post',
+      url: API_URL,
+      headers: { 
+        'Authorization': 'Basic bW9iaWxlOnBpbg=='
+      },
+      data : data
+    };
+    return axios(config)
+        .then(function (response){
+        if (response.data.access_token) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
+        return response.data;
+      });
+  }
+
+  logout() {
+    localStorage.removeItem("user");
+  }
+
+  register(username, email, password) {
+    return axios.post(API_URL + "signup", {
+      username,
+      email,
+      password
+    });
+  }
+
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem('user'));;
+  }
+}
+
+export default new AuthService();
